817c5fa74ddf929513e4ef339cde0f4729298954
Lalocal1210 <125925597+Lalocal1210@users.noreply.github.com>
2025-05-16 20:23:06 -0600

    progrmas

---
+++ Practica1/ejercicio1.js
@@ -0,0 +1,9 @@
+//  variables
+const nombre = "Armando";   
+const edad = 25;            
+
+// mensaje
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+// mensaje en la consola
+console.log(saludo);

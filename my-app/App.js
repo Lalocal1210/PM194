731bd694f704a0cd45debd8f82020a01b8cc38c1
Lalocal1210 <125925597+Lalocal1210@users.noreply.github.com>
2025-06-13 20:17:54 -0600

    practica terminada

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+const Texto = ({ estiloFondo }) => {
+  const [contenido, setContenido] = useState('Hola mundo RNative');
+  const actualizarTexto = () => setContenido('Estado actualizado del Text');
+  return (
+    <Text style={[styles.textoBlanco, estiloFondo]} onPress={actualizarTexto}>
+      {contenido}
+    </Text>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Texto estiloFondo={styles.red} />
+      <Texto estiloFondo={styles.blue} />
+      <Texto estiloFondo={styles.green} />
+      
     </View>
   );
 }
@@ -14,7 +27,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'baseline',
     justifyContent: 'center',
+    flexDirection:'row-reverse'
+  
+  },
+  textoBlanco: {
+    color: 'white',
+    fontSize: 18,
+    margin: 10,
+    width: 100,
+    textAlign: 'center',
+    padding: 10,
+   
+  },
+  red: {
+    backgroundColor: 'red',
+  },
+  blue: {
+    backgroundColor: 'blue',
+  },
+  green: {
+    backgroundColor: 'green',
   },
 });
+
